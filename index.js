cd4bf742bd4147aea27a73112652004b9a4e57e3
moringa <oscarokumu47@gmail.com>
2022-08-25 09:41:33 +0300

    modify package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee ={
+    name: 'Oscar',
+    streetAddress :'Thika Road'
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let employees = {...employee};
+    employees[key]=value;
+    return employees;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = 'Sam';
+    employee.streetAddress = "12 Broadway";
+    let employees = {...employee};
+    employees[key] =value
+    return employees;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee};
+    delete newEmployee.name;
+    delete newEmployee.streetAddress;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    let newEmployee = employee;
+    delete newEmployee.name;
+    delete newEmployee.streetAddress;
+    return newEmployee;
+}
